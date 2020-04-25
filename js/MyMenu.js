032802fefbb641628fe580395646400fe28556b5
parmasdev <patrice.armas@gmail.com>
2020-04-25 10:33:40 +0200

    Nouveau menu principal

--- js/MyMenu.js
+++ js/MyMenu.js
@@ -32,22 +32,17 @@ function MyMenu(props) {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem onClick={handleClose}>
-          <Link href="text_to_picts_material_ui_CDN_V2.html">
-            <Icon>create</Icon>&nbsp;Ecrire une histoire
-          </Link>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <Link href="LIRE_HISTOIRE_react_material_ui_CDN_V2.html">
-            <Icon>menu_book</Icon>&nbsp;Lire une histoire
-          </Link>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <Link href="GAME_react_material_ui_CDN_V3.html">
-            <Icon>sports_esports</Icon>&nbsp;Game - Remettre l'histoire dans l'ordre
-          </Link>
-        </MenuItem>
-      </Menu>
+      <MenuItem onClick={handleClose}>
+        <Link href="SelectStory.html">
+          <Icon>menu</Icon>&nbsp;Menu principal
+        </Link>
+      </MenuItem>
+      <MenuItem onClick={handleClose}>
+        <Link href="text_to_picts_material_ui_CDN_V2.html">
+          <Icon>create</Icon>&nbsp;Ecrire une histoire
+        </Link>
+      </MenuItem>
+    </Menu>
     </div>
   );
 
